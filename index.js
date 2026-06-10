d48078e0b74020d0dcb39c8afdd7de9216763112
11lynx <malte.niehues2013@gmail.com>
2026-06-10 20:53:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const mineflayer = require('mineflayer');
 
-const bot = mineflayer.createBot({
-  host: 'Skibidi-7004.aternos.me',
-  port: 18824,
-  username: 'Slobot00'
-});
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'Skibidi-7004.aternos.me',
+    port: 18824,
+    username: 'Slobot00',
+    auth: 'offline'
+  });
 
-bot.on('spawn', () => {
-  console.log('Bot ist online!');
-  bot.chat('Ich bin da!');
-});
+  bot.on('spawn', () => {
+    console.log('Bot ist online!');
+    bot.chat('Ich bin da!');
+  });
 
-bot.on('error', (err) => {
-  console.log('Fehler:', err.message);
-});
+  bot.on('kicked', (reason) => {
+    console.log('GEKICKT:', reason);
+  });
 
-bot.on('end', () => {
-  console.log('Verbindung verloren');
-});
+  bot.on('error', (err) => {
+    console.log('FEHLER:', err);
+  });
+
+  bot.on('end', () => {
+    console.log('Verbindung verloren → Reconnect in 5s');
+    setTimeout(createBot, 5000);
+  });
+}
+
+createBot();
