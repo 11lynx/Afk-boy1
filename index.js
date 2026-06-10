19597923ca307afccaae2da3145c6c7cf5dbc5bc
11lynx <malte.niehues2013@gmail.com>
2026-06-10 21:52:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const mineflayer = require('mineflayer');
 
-function start() {
+function startBot() {
   const bot = mineflayer.createBot({
     host: 'Skibidi-7004.aternos.me',
     port: 18824,
     username: 'Slobot00',
     auth: 'offline',
-    version: '1.20.4'
+    version: '1.21.4'
   });
 
   bot.on('spawn', () => {
-    console.log('Bot online');
+    console.log('Bot ist online!');
+    bot.chat('Ich bin da!');
+  });
+
+  bot.on('error', (err) => {
+    console.log('FEHLER:', err);
   });
 
   bot.on('end', () => {
-    console.log('Reconnect...');
-    setTimeout(start, 5000);
+    console.log('Reconnect in 5 Sekunden...');
+    setTimeout(startBot, 5000);
   });
 }
 
-start();
+startBot();
